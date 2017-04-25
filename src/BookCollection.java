5e6b0e793873b2031e16ce04d5cf9a398f832408
LheaJCopeland <LheaCopeland@gmail.com>
2017-04-25 17:33:06 -0400

    Adding BookCollection class with Antonella & Maurices help

---
+++ src/BookCollection.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+/**
+ * Created by LheaJLove on 4/25/2017.
+ */
+public class BookCollection {
+
+
+    ArrayList<Book> collection = new ArrayList<Book>();
+
+    public ArrayList<Book> startInitialCollection (ArrayList collection, Book book){
+        collection.add(new Book("The Elegant Universe", "Brian Green", true));
+        collection.add(new Book("The Bluest Eye", "Toni Morrison", true));
+        collection.add(new Book("My House", "Nikki Giovanni", true));
+        collection.add(new Book("We Should All Be Feminists", "Chimamanda Ngozi Adichie", true));
+        collection.add(new Book("A Game of Thrones", "George R. R. Martin", true));
+        collection.add(new Book("Harry Potter", "J.K. Rowling", true));
+        collection.add(new Book("Martin The Warrior", "Brian Jacques", true));
+        collection.add(new Book("Lord of the Rings", "J.R.R. Tolkien", true));
+        collection.add(new Book("Ponniyen Selvan", "Kalki", true));
+        collection.add(new Book("Thanneer Desam", "Vairamuthu", true));
+        collection.add(new Book("Karuvaachi Kaviyam ", "Vairamuthu", true));
+        collection.add(new Book("Udaiyar", "Balakumaran", true));
+    return collection;
+    }
+
+    public Book addToList(String name, String author, boolean status) {
+        Book book = new Book(name, author, status);
+        collection.add(book);
+        return book;
+    }
+
+    public Book changeStatus(String name, String author, boolean status){
+        Book book = new Book (name, author, status);
+        collection.get(0).setStatus(true);
+        return book;
+    }
+
+
+}
