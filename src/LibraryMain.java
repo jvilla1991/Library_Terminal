10a9eb79578612574e482bd493480f11562a67ed
LheaJCopeland <LheaCopeland@gmail.com>
2017-04-25 16:36:23 -0400

    Added Array List

--- src/LibraryMain.java
+++ src/LibraryMain.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -15,6 +16,24 @@ public class LibraryMain {
         int choice = 1;
         String choice1 = "yes";
         Book book = new Book("name", "author", true);
+
+        ArrayList<Book> collection = new ArrayList<Book>();
+        collection.add(new Book("The Elegant Universe", "Brian Green", true));
+        collection.add(new Book("The Bluest Eye", "Toni Morrison", true));
+        collection.add(new Book("My House", "Nikki Giovanni", true));
+        collection.add(new Book("We Should All Be Feminists", "Chimamanda Ngozi Adichie", true));
+        collection.add(new Book("A Game of Thrones", "George R. R. Martin", true));
+        collection.add(new Book("Harry Potter", "J.K. Rowling", true));
+        collection.add(new Book("Martin The Warrior", "Brian Jacques", true));
+        collection.add(new Book("Lord of the Rings", "J.R.R. Tolkien", true));
+        collection.add(new Book("Ponniyen Selvan", "Kalki", true));
+        collection.add(new Book("Thanneer Desam", "Vairamuthu", true));
+        collection.add(new Book("Karuvaachi Kaviyam ", "Vairamuthu", true));
+        collection.add(new Book("Udaiyar", "Balakumaran", true));
+
+
+        collection.get(0).setStatus(true);
+
 
         System.out.println("Welcome to the SJL Library");
 
