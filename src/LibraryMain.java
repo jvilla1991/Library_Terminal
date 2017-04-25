3a043e125299addad423a658a28074c54ab594cd
LheaJCopeland <LheaCopeland@gmail.com>
2017-04-25 13:59:02 -0400

    Lhea's first commit of LibraryMain

--- src/LibraryMain.java
+++ src/LibraryMain.java
@@ -1,9 +1,50 @@
+import java.util.Scanner;
+
 /**
  * Created by Admin on 4/25/2017.
  */
 public class LibraryMain {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
         System.out.println("Saritha");
+
+        Scanner scan = new Scanner(System.in);
+        int choice = 1;
+        String choice1 = "yes";
+        Book book = new Book("name", "author");
+
+        System.out.println("Welcome to the SJL Library");
+
+        do {
+            System.out.println("Would you like to:");
+            System.out.println("1. Display the Book Collection");
+            System.out.println("2. Search by Title Keyword");
+            System.out.println("3. Search by Author");
+            System.out.println("4. Checkout a Book");
+            System.out.println("5. Return a Book");
+            choice = scan.nextInt();
+            scan.nextLine();
+
+            switch (choice){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+            System.out.println("Would you like to continue?");
+            choice1 = scan.nextLine();
+        } while (choice1.equalsIgnoreCase("yes"));
+        System.out.println("Thank you. Please come again!");
+
+
+
     }
 }
