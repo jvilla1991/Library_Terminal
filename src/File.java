387f4d45b142d467669fb55e1c6a67ef0c821192
jvilla1991 <villajeremy1991@gmail.com>
2017-04-25 13:42:22 -0400

    initial file class

---
+++ src/File.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 4/25/2017.
+ */
+public class File {
+
+    public static void readFromFile() {
+        Path filePath = Paths.get("books.txt");
+        java.io.File textFile = filePath.toFile();
+        try {
+            FileReader r = new FileReader(textFile);
+            BufferedReader reader = new BufferedReader(r);
+            try {
+                String line = reader.readLine();
+
+                while (line != null) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToFile(Scanner scnr) {
+        scnr.nextLine();
+        System.out.println("Please enter a book name: ");
+        String country = scnr.nextLine();
+        Path writePath = Paths.get("books.txt");
+        java.io.File countryFiles = writePath.toFile();
+
+        try {
+            PrintWriter out = new PrintWriter(new FileOutputStream(countryFiles, true));
+            out.println(book);
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
